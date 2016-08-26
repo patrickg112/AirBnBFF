b404d512397b03093a1d780b3b1537ab5d77e6ce
Anthony Ciacci <ciacci1234@gmail.com>
2016-08-26 12:01:20 -0400

    first draft of setup for recommendify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+#Emotional Analysis Helpers
+
+  def traits(user)
+      traits = []
+      traits << emo_analysis(user.openness, "open")
+      traits << emo_analysis(user.conscientiousness, "conscientious")
+      traits << emo_analysis(user.extraversion, "extraverted")
+      traits << emo_analysis(user.agreeableness, "agreeable")
+      traits << emo_analysis(user.emotional_range, "neurotic")
+      traits
+  end
+
+  def emo_analysis(emo_val, emo_name)
+    case emo_val
+    when emo_val <= 2
+      "not very #{emo_name}"
+    when emo_val = 3
+      "#{emo_name}"
+    when emo_val >= 4
+      "very #{emo_name}"
+    else
+      "inconclusive"
+    end
+  end
+
 end
